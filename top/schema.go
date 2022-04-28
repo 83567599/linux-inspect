8575ca02ae65284a5ae575b720f29f88053162e4
zjyang <83567599@qq.com>
2022-04-28 17:36:33 +0800

    Update schema.go

--- top/schema.go
+++ top/schema.go
@@ -3,7 +3,7 @@ package top
 import (
 	"reflect"
 
-	"github.com/gyuho/linux-inspect/schema"
+	"github.com/83567599/linux-inspect/schema"
 )
 
 // RowSchema represents a row in 'top' command output.
